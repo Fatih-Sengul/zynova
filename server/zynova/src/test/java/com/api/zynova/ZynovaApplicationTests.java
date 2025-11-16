6788f3ac98793399c35104261178371f88e24b4a
Fatih Şengül <111802646+Fatih-Sengul@users.noreply.github.com>
2025-11-17 00:14:29 +0300

    Initial commit

---
+++ server/zynova/src/test/java/com/api/zynova/ZynovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.zynova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZynovaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
